6be59874e574f7d4623edc3b14b1cdbdfcc05c55
Lucas Ramony <lramony>
2020-12-17 23:40:05 -0300

    subjec implicito e explicito

--- spec/calculator/calculatorSubject_spec.rb
+++ spec/calculator/calculatorSubject_spec.rb
@@ -1,9 +1,10 @@
 require 'calculator'
+#rspec ./spec/calculator/calculatorSubject_spec.rb:23 < para executar o teste apenas de uma linhda determinada
 
 #Usamos o subject para não precisar ficar instanciando varias
-#Primeira forma de fazer
-describe Calculator do
-    context "vou querer um testa para o metodo sun" do       
+#Primeira forma de fazer "SubjectImplicito"
+describe Calculator, "Sobre calculadora" do
+    context "#sum" do       
         it {
             #calc = Calculator.new
             result = subject.sum(5,7)
@@ -18,7 +19,26 @@ describe Calculator do
     end
 end
 
-#Segunda forma de fazer
+#O describe mais interno vence
+describe Calculator, "ClassePai" do
+    describe Calculator, "ClassFilha" do
+        context "#sum" do       
+            it {
+                #calc = Calculator.new
+                result = subject.sum(5,7)
+                expect(result).to eq(12)
+            }
+
+            it {
+                #calc = Calculator.new
+                result = subject.sum(-5,7)
+                expect(result).to eq(2)
+            }
+        end
+    end    
+end
+
+#Segunda forma de fazer "SubjectExplicito"
 describe 'Classe Calculadora 3' do
     subject(:calc) { Calculator.new() } #classe reservada
 
